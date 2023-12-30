06637f6171ccdd0c817d38fac39dc7238964e57f
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-12-30 06:47:24 -0500

    Header added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './Header.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   )
 }
-
-export default App
---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,30 @@
+export default function Header() {
+    return (
+        <header className="header" role="banner" id="top">
+            <div className="row">
+                <nav className="nav" role="navigation">
+                    <ul className="nav__items">
+                        <li className="nav__item">
+                            <a href="#work" className="nav__link">Work</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">About</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="header__text-box row">
+                <div className="header__text">
+                    <h1 className="heading-primary">
+                        <span>Jonah Foster</span>
+                    </h1>
+                    <p>Full-Stack Developer</p>
+                    <a href="#contact" className="btn btn--pink">Get in touch</a>
+                </div>
+            </div>
+        </header>
+    )
+}
