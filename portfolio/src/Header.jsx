b4e0e957853e860db1ddbb57da790ab394acf33b
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-12-30 10:59:50 -0500

    Animations added to projects

--- portfolio/src/Header.jsx
+++ portfolio/src/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
                     <h1 className="heading-primary">
                         <span>Jonah Foster</span>
                     </h1>
-                    <p>Full-Stack Developer</p>
+                    <p>Where imagination meets technology</p>
                     <a href="#contact" className="btn btn--pink">Get in touch</a>
                 </div>
             </div>
--- portfolio/src/Projects.jsx
+++ portfolio/src/Projects.jsx
@@ -2,96 +2,101 @@ import moviePickerImg from './assets/popchoice.jpg'
 import planetsImg from './assets/planetsapp.jpg'
 import hologramImg from './assets/Jonah Foster Hologram.jpg'
 import githubIcon from './assets/github.svg'
+import PropTypes from 'prop-types'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 export default function Projects() {
+
+    const projects = [
+        {
+            title: "Movie Picker",
+            img: moviePickerImg,
+            description: "An OpenAI powered site to suggest movies based on user-preferences. Supports both individuals and groups.",
+            attributeList: ["React", "CSS", "OpenAI API", "Supabase Embeddings Database"],
+            siteLink: "https://peaceful-piroshki-a2d371.netlify.app/",
+            githubLink: "https://github.com/JonahFoster/scrimba-popchoice",
+        },
+        {
+            title: "The Planets",
+            img: planetsImg,
+            description: "Explore the Solar System in this React design project.",
+            attributeList: ["React", "CSS", "Responsive", "Dynamic Content"],
+            siteLink: "https://jonah-planet-facts.netlify.app/",
+            githubLink: "https://github.com/JonahFoster/planet-facts",
+        },
+        {
+            title: "Hologram",
+            img: hologramImg,
+            description: "An Instagram clone from a galaxy far, far away.",
+            attributeList: ["HTML5", "CSS", "JavaScript", "The Force"],
+            siteLink: "https://starwars-hologram.netlify.app/",
+            githubLink: "https://github.com/JonahFoster/scrimba-instagram-clone",
+        },
+    ]
+
     return (
         <section className="work" id="work">
             <div className="row">
                 <h2>My Work</h2>
                 <div className="work__boxes">
-                    <div className="work__box">
-                        <div className="work__text">
-                            <h3>Movie Picker</h3>
-                            <p>
-                                An OpenAI powered site to suggest movies based on user-preferences. Supports both individuals and groups.
-                            </p>
-                            <ul className="work__list">
-                                <li>React</li>
-                                <li>CSS</li>
-                                <li>OpenAI API</li>
-                                <li>Supabase Embeddings Database</li>
-                            </ul>
-
-                            <div className="work__links">
-                                <a href="https://peaceful-piroshki-a2d371.netlify.app/" target="_blank" rel="noreferrer" className="link__text">
-                                    Visit Site <span>&rarr;</span>
-                                </a>
-                                <a href="https://github.com/JonahFoster/scrimba-popchoice" title="View Source Code" target="_blank" rel="noreferrer">
-                                    <img src={githubIcon} className="work__code" alt="GitHub Icon" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="work__image-box">
-                            <img src={moviePickerImg} className="work__image" alt="Jonah Foster Movie Picker" />
-                        </div>
-                    </div>
-
-                    <div className="work__box">
-                        <div className="work__text">
-                            <h3>The Planets</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-                                eius.
-                            </p>
-                            <ul className="work__list">
-                                <li>React</li>
-                                <li>CSS</li>
-                                <li>Responsive</li>
-                                <li>Dynamic Content</li>
-                            </ul>
-
-                            <div className="work__links">
-                                <a href="https://jonah-planet-facts.netlify.app/" className="link__text">
-                                    Visit Site <span>&rarr;</span>
-                                </a>
-                                <a href="https://github.com/JonahFoster/planet-facts">
-                                    <img src={githubIcon} className="work__code" title="View Source Code" alt="GitHub Icon" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="work__image-box">
-                            <img src={planetsImg} className="work__image" alt="Jonah Foster The Planets project" />
-                        </div>
-                    </div>
-
-                    <div className="work__box">
-                        <div className="work__text">
-                            <h3>Hologram</h3>
-                            <p>
-                                An Instagram clone from a galaxy far, far away.
-                            </p>
-                            <ul className="work__list">
-                                <li>HTML5</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>The Force</li>
-                            </ul>
-
-                            <div className="work__links">
-                                <a href="https://starwars-hologram.netlify.app/" className="link__text">
-                                    Visit Site <span>&rarr;</span>
-                                </a>
-                                <a href="https://github.com/JonahFoster/scrimba-instagram-clone">
-                                    <img src={githubIcon} className="work__code" title="View Source Code" alt="GitHub" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="work__image-box">
-                            <img src={hologramImg} className="work__image" alt="Jonah Foster Hologram Project" />
-                        </div>
-                    </div>
+                    {projects.map((project, index) => (
+                        <CreateProject key={index} project={project} />
+                    ))}
                 </div>
             </div>
-            </section>
+        </section>
     )
+}
+
+function CreateProject({ project }) {
+    const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.3 })
+    const projectVariants = {
+        offscreen: { opacity: 0, x: -100 },
+        onscreen: { opacity: 1, x: 0 }
+    }
+
+    return (
+        <motion.div
+            ref={ref}
+            initial="offscreen"
+            animate={inView ? "onscreen" : "offscreen"}
+            variants={projectVariants}
+            className="work__box"
+        >
+            <div className="work__text">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <ul className="work__list">
+                    {project.attributeList.map((attribute) => {
+                        return (
+                            <li key={attribute}>{attribute}</li>
+                        )
+                    })}
+                </ul>
+                <div className="work__links">
+                    <a href={project.siteLink} target="_blank" rel="noreferrer" className="link__text">
+                        Visit Site <span>&rarr;</span>
+                    </a>
+                    <a href={project.githubLink} title="View Source Code" target="_blank" rel="noreferrer">
+                        <img src={githubIcon} className="work__code" alt="GitHub Icon" />
+                    </a>
+                </div>
+            </div>
+            <div className="work__image-box">
+            <motion.img src={project.img} whileHover={{ scale: 1.1 }} className="work__image" alt={`Jonah Foster ${project.title}`} />
+            </div>
+        </motion.div>
+    )
+}
+
+CreateProject.propTypes = {
+    project: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        attributeList: PropTypes.arrayOf(PropTypes.string).isRequired,
+        siteLink: PropTypes.string.isRequired,
+        githubLink: PropTypes.string.isRequired,
+    }).isRequired,
 }
